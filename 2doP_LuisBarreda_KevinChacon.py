f53f2992b45f23bb9386ff2a457b52ca867c533f
kevinch43 <67874304+kevinch43@users.noreply.github.com>
2021-06-16 18:06:28 -0600

    Update 2doP_LuisBarreda_KevinChacon.py

--- 2doP_LuisBarreda_KevinChacon.py
+++ 2doP_LuisBarreda_KevinChacon.py
@@ -81,3 +81,66 @@ def openinst():
 """Top Players Window______________________________________________________________________________________________________________________________
 ___________________________________________________________________________________________________________________________________________
 ___________________________________________________________________________________________________________________________________________"""
+
+"""Main Window______________________________________________________________________________________________________________________________
+___________________________________________________________________________________________________________________________________________
+___________________________________________________________________________________________________________________________________________"""
+mw = tk.Tk() #Creates the main window
+name = StringVar() #Takes the name of the user for the top 7 list
+
+mw.title("Operation Moon Light 2") #Title of the principal window
+mw.geometry("500x300") #Size of the principal window
+mw.resizable(False, False)
+
+"""Images___________________________________________________________________________________________________________"""
+
+bg = PhotoImage(file = 'img/background1.gif')#create a image background 
+bg_label= tk.Label(mw, image= bg)
+bg_label.place(x=0,y=0)
+
+bg_about = PhotoImage(file = 'img/bg_about.gif')#create a image background
+bg_instructions = PhotoImage(file = 'img/bg_inst.gif')#create a image background
+
+space = PhotoImage(file = 'img/background.gif')#create a image background
+space = space.subsample(1)
+space = space.zoom(2)
+
+easy = PhotoImage(file = 'img/easy.png')#create a easy image 
+medium = PhotoImage(file = 'img/medium.png')#create a medium image  
+hard = PhotoImage(file = 'img/hard.png')#create a hard image
+
+about = PhotoImage(file = 'img/about.png')#create a about image 
+top = PhotoImage(file = 'img/top.png')#create a top image  
+instructions = PhotoImage(file = 'img/instructions.png')#create a isntructions image
+back = PhotoImage(file = 'img/back.png')#create a back image
+arrows = PhotoImage(file = 'img/arrows.png')#create a arrows image 
+
+ufoimg = PhotoImage(file = 'img/ufo.png')
+ufoimg1 = PhotoImage(file = 'img/ufo1.png')
+userimg = PhotoImage(file = 'img/player.png')
+
+"""Entries___________________________________________________________________________________________________________"""
+
+user_name = tk.Entry(mw, textvariable = name, font = ("Comic Sans MS", 14), width = 10) #Entry widget for the user to write his/her name
+user_name.place(x=185, y=65)
+
+"""Labels___________________________________________________________________________________________________________"""
+usernameinstruction = tk.Label(mw, font = ("Comic Sans MS", 12), text = "Enter your name to save your results", fg = "ghostwhite", bg = "#07070a"). place(x = 115, y = 95) #Label with the title of the game
+
+"""Buttons___________________________________________________________________________________________________________"""
+level1 = tk.Button(mw, image= easy, bg = "black", command = game1) .place(x=135, y=130) #Opens the level 1 window
+level2 = tk.Button(mw, image= medium, bg = "black", command = game2) .place(x=215, y=130) #Opens the level 2 window
+level3 = tk.Button(mw, image= hard, bg = "black", command = game3) .place(x=295, y=130) #Opens the level 3 window
+
+btn_about = tk.Button(mw, image= about, command = openaboutw,  bg = "black") .place(x=135, y=190) #Opens the about window
+btn_top = tk.Button(mw, image= top, bg = "black", command = game2) .place(x=215, y=190) #Opens the top window
+btn_instructions = tk.Button(mw, image= instructions, bg = "black",command = openinst) .place(x=295, y=190) #Opens the instructions window
+
+second = 0
+userlife = 3
+score = 0
+score2 = 0
+score3 = 0
+score4 = 0
+
+mw.mainloop()
