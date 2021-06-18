ec2702608ee428cf183e9a5ae79d542044c25ae5
luisale06 <86024351+luisale06@users.noreply.github.com>
2021-06-18 17:51:58 -0600

    Add files via upload

---
+++ QuickSort.py
@@ -0,0 +1,54 @@
+import random
+import time
+
+'''Resultados al final del código'''
+
+def partition(arr, low, high):
+    i = (low-1)         # index of smaller element
+    pivot = arr[high]     # pivot
+  
+    for j in range(low, high):
+  
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+  
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+  
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+  
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+  
+# Function to do Quick sort
+  
+  
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+  
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+  
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+'''Pruebas al algoritmo'''
+#Listas generadas
+list10k = random.sample(range(0, 100000), 10000) #lista 10 000 elementos
+
+#Pruebas de tiempo
+start_time = time.time()
+n = len(list10k) - 1
+sort = quickSort(list10k, 0, n)
+end_time = time.time() - start_time
+print(end_time)
